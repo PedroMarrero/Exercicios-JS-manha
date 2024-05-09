d1233b65eb62c767ec40f7af1059dc2db104eb57
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-09 10:30:32 -0300

    Feat: exercicio 3

--- ex2.js
+++ ex2.js
@@ -3,25 +3,29 @@
 // semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
 // 2 representa terça-feira e assim por diante.
 
-let semana = prompt("Digite um numero de 1 a 7: ");
+// let semana = prompt("Digite um numero de 1 a 7: ");
 
-if (semana == "1")
-    console.log("Segunda");
+// if (semana == "1")
+//     console.log("Segunda");
 
-    if (semana == "2")
-    console.log("Terça");
+// if (semana == "2")
+//     console.log("Terça");
 
-    if (semana == "3")
-    console.log("Quarta");
+// if (semana == "3")
+//     console.log("Quarta");
 
-    if (semana == "4")
-    console.log("Quinta");
+// if (semana == "4")
+//     console.log("Quinta");
 
-    if (semana == "5")
-    console.log("Sexta");
+// if (semana == "5")
+//     console.log("Sexta");
 
-    if (semana == "6")
-    console.log("Sábado");
+// if (semana == "6")
+//     console.log("Sábado");
 
-    if (semana == "7")
-    console.log("Domingo");
+// if (semana == "7")
+//     console.log("Domingo");
+
+// if (semana > 7){
+//     console.log ("Esse dia não existe")
+// }
---
+++ ex3.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let numero = parseInt(prompt("Digite um número: "))
+
+if (numero /2 == 1 ){
+    console.log("Esse número é par" )
+}
+ else{
+    console.log("Esse número é ímpar" )
+ }
