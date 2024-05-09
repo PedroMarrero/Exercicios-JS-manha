6c67d2330d3231310ca7ea55113e89c330b783fd
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-09 08:43:36 -0300

    Feat: exercicio 1

---
+++ ex1.js
@@ -0,0 +1,14 @@
+// Exercício 1: Verificação de login
+// Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+// nome de usuário é admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+// foi bem-sucedido ou não.
+
+let username = prompt("Digite um nome de usuário: ")
+
+let senha = prompt("Digite a senha: ")
+
+if (username == "admin" && senha == "senha123") {
+    console.log("Login bem sucedido");
+} else {
+    console.log("Você não tem permissão de acesso");
+}
