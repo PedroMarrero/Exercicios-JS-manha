c7e7af8a643e1be565d0ed117866535dd321648b
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-10 10:51:21 -0300

    Feat: exercicio 6

--- ex5.js
+++ ex5.js
@@ -3,20 +3,20 @@
 // crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
 // não.
 
-let primeiroNumero = parseInt(prompt("Digite o primeiro número:"))
-let segundoNumero = parseInt(prompt("Digite o segundo número:"))
-let terceiroNumero = parseInt(prompt("Digite o terceiro número:"))
+// let primeiroNumero = parseInt(prompt("Digite o primeiro número:"))
+// let segundoNumero = parseInt(prompt("Digite o segundo número:"))
+// let terceiroNumero = parseInt(prompt("Digite o terceiro número:"))
 
-if(primeiroNumero < segundoNumero && segundoNumero < terceiroNumero){
-console.log("os números estão em ordem crescente")
-}
-else{(primeiroNumero > segundoNumero && segundoNumero > terceiroNumero)
-    console.log("Os números não estão em ordem crescente")
+// if(primeiroNumero < segundoNumero && segundoNumero < terceiroNumero){
+// console.log("os números estão em ordem crescente")
+// }
+// else{(primeiroNumero > segundoNumero && segundoNumero > terceiroNumero)
+//     console.log("Os números não estão em ordem crescente")
 
-}
+// }
 
-else{
-    alert("Não está em ordem")
-}
+// else{
+//     alert("Não está em ordem")
+// }
 
 // desafio: informe se os números estão em ordem crescente / decrescente / náo ordenado
