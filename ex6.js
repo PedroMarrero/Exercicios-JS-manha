590456f3bc4f93aaf8193b06c2bdc2f314bc210e
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-13 08:24:59 -0300

    Feat: exercicio 7

--- ex6.js
+++ ex6.js
@@ -3,16 +3,16 @@
 // de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
 
 
-let anoNascimento = parseInt(prompt("Digite o ano de nascimento: "))
-// let anoAtual = 2024
-let anoAtual = new Date().getFullYear()
-let idade = anoAtual - anoNascimento
+// let anoNascimento = parseInt(prompt("Digite o ano de nascimento: "))
+// // let anoAtual = 2024
+// let anoAtual = new Date().getFullYear()
+// let idade = anoAtual - anoNascimento
 
-if (idade >= 18) {
-    console.log("Você é maior de idade")
-    console.log("Sua idade é " + idade)
-}
-else {
-    console.log("Você é menor de idade")
-    console.log("Sua idade é " + idade)
-}
+// if (idade >= 18) {
+//     console.log("Você é maior de idade")
+//     console.log("Sua idade é " + idade)
+// }
+// else {
+//     console.log("Você é menor de idade")
+//     console.log("Sua idade é " + idade)
+// }
