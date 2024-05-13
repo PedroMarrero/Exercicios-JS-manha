7435325d7d0ca077abfe39afd07d874203a44fbf
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-13 10:50:40 -0300

    Feat: exercicios estrutura de repetição

---
+++ loop/exemplo.js
@@ -0,0 +1,81 @@
+//for = loop contado //for = loop contado //for = loop contado //
+
+// for (let num = 0; num <= 10; num++) {
+//     console.log(num);
+// }
+
+//Contagem regressiva // Contagem regressiva // Contagem regressiva //
+
+// for (let cont = 10; cont >= 0; cont--) {
+//     console.log(cont);
+// }
+
+//While - loop condicional //While - loop condicional //While - loop condicional
+
+// let salario = 1000;
+
+// while (salario < 5000) {
+//     salario += 100
+//     console.log("O salario ainda é: "+ salario)
+// }
+
+// let saldo = 1000;
+
+// do {
+//     console.log("Esse é o saldo" + saldo);
+//     saldo += 100;
+
+// }while(saldo < 5000);
+
+//Array //Array //Array //Array //Array //Array //Array //Array
+
+// let frutas = ["Banana", "Laranja", "Maça", "Pitaya", "Melancia"]
+
+// console.log(frutas[3])
+
+// frutas.forEach(fruta =>{
+//     console.log("A fruta é: " + fruta)
+// })
+
+// for (let indice = 0; indice < frutas.length; indice++) {
+//     console.log(frutas[indice])
+//     if (frutas[indice] == "Pitaya")
+//         console.log("Eu gosto de " + frutas[indice])
+// }
+
+
+
+// FOR
+
+// 1. Imprimir números de 1 a 10 utilizando o loop for
+
+// 2. Imprimir números pares de 1 a 10.
+
+// 3. Faça um programa que imprima os números ímpares de 0 a 50;
+
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+
+
+// WHILE
+
+
+
+// 6. Imprimir números pares de 0 a 20 utilizando o loop while
+
+// 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+
+// 8. Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+
+// 9. Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
