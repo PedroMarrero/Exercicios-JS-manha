98d0ba484f42f48398a6974f2be084c5f6a9aaa7
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-14 09:51:29 -0300

    Feat: exercicio 5

--- loop/ex4.js
+++ loop/ex4.js
@@ -2,15 +2,15 @@
 // qualquer escolhido pelo usuário até o 10.
 
 // Pedir p o usuario que colocar um número
-const numero = parseInt(prompt("Coloque um numero para realizarmos a sua tabuada ate o 10:"));
+// const numero = parseInt(prompt("Coloque um numero para realizarmos a sua tabuada ate o 10:"));
 
-// Verificar si se o número válido
-if (isNaN(numero)) {
-    console.log("Por favor, ingrese un número válido.");
-} else {
-    // Imprimir a tabuada do número ingresado ate 10
-    console.log(`Tabuada de ${numero}:`);
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${numero} x ${i} = ${numero * i}`);
-    }
-}
+// // Verificar si se o número válido
+// if (isNaN(numero)) {
+//     console.log("Por favor, ingrese un número válido.");
+// } else {
+//     // Imprimir a tabuada do número ingresado ate 10
+//     console.log(`Tabuada de ${numero}:`);
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${numero} x ${i} = ${numero * i}`);
+//     }
+// }
