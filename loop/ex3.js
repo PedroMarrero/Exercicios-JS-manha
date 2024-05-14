a27a82aff9741ad6530ad31dc8416a6e3c9621e4
PEDRO MARRERO OLIVEIRA DA SILVA <44985891819@info.local>
2024-05-14 09:46:50 -0300

    Feat: exercicio 4

--- loop/ex3.js
+++ loop/ex3.js
@@ -1,5 +1,5 @@
 // 3. Faça um programa que imprima os números ímpares de 0 a 50;
 
-for (let impar = 1; impar <= 50; impar += 2) {
-    console.log(impar);
-}
+// for (let impar = 1; impar <= 50; impar += 2) {
+//     console.log(impar);
+// }
